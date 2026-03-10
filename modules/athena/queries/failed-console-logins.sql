0864cadf9825c71460bc729e1bee6a91ef8d22d8
kalabtech <235978118+kalabtech@users.noreply.github.com>
2026-03-10 16:43:59 +0000

    fix(query): remove duplicate <ORDER BY eventtime DESC>

--- modules/athena/queries/failed-console-logins.sql
+++ modules/athena/queries/failed-console-logins.sql
@@ -12,6 +12,6 @@ FROM cloudtrail_logs
 WHERE eventname = 'ConsoleLogin'
 AND errorcode = 'Failed'
     AND timestamp >= date_format(current_date - interval '3' month, '%Y/%m/%d')
-    AND timestamp <= date_format(current_date, '%Y/%m/%d')ORDER BY eventtime DESC
+    AND timestamp <= date_format(current_date, '%Y/%m/%d')
 ORDER BY eventtime DESC
 LIMIT 100;
