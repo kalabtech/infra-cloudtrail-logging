6431e8962389ec18de930226a7df620dc316a4c1
kalabtech <235978118+kalabtech@users.noreply.github.com>
2026-03-10 08:08:42 +0000

    fix(athena): Adds missing rule to Athena workgroup not included in (#8)

--- modules/athena/athena.tf
+++ modules/athena/athena.tf
@@ -10,7 +10,12 @@ resource "aws_athena_workgroup" "this" {
     result_configuration {
       output_location = "s3://${var.s3_bucket_id}/athena-results/"
 
-      # NOTE: Encrypts query results with the same KMS key used for CloudTrail logs
+      # NOTE: Ensure to be the owner of the destination bucket.
+      acl_configuration {
+        s3_acl_option = "BUCKET_OWNER_FULL_CONTROL"
+      }
+
+      # NOTE: Encrypts query results with the same KMS key used for CloudTrail logs.
       encryption_configuration {
         encryption_option = "SSE_KMS"
         kms_key_arn       = var.kms_key_arn
